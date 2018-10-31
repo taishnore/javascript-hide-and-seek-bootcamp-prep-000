59eb305f2cfe204c602b0c4b3f980e67f95e0587
Taimur Shah <tshah@scu.edu>
2018-10-31 19:03:37 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+    const here = document
+		.getElementById("grand-node")
+		.querySelectorAll("div");
+    for (let i = 0; i < here.length; i++) {
+        if (i === here.length-1) {
+            return here[i]
+        }
+    }
+}
+
+function increaseRankBy(n) {
+    let tab = document
+		.getElementById("app")
+		.querySelectorAll("ul.ranked-list li");
+	for (let i = 0; i < tab.length; i++) {
+        tab[i].innerHTML = (parseInt(tab[i].innerHTML) + n)
+    }
+}
+
+
+
+
 
