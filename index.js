e8f936fb01a70b8fdf0774ecc96c31ef50da701b
Taimur Shah <tshah@scu.edu>
2018-10-31 18:15:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function deepestChild() {
+    const here = document
+		.getElementById("grand-node")
+		.querySelectorAll("div");
+    for (let i = 0; i < here.length; i++) {
+        if (i === here.length-1) {
+            return here[i]
+        }
+    }
+}
 
